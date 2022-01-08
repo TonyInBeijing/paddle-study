96e1408243589b665517a9fb3c5ad6ab06276ff6
TonyInBeijing <yuehaowei1126@gmail.com>
2022-01-08 16:13:21 +0800

    手写数字识别-数据处理

--- chapter2-1-2.py
+++ chapter2-1-2.py
@@ -9,69 +9,117 @@ import json
 import random
 import numpy as np
 
-datafile = "./work/mnist.json.gz"
-print("loading mnist dataset from {} ......".format(datafile))
+# 封装数据读取与处理函数
 
-# 加载 json 数据文件
-data = json.load(gzip.open(datafile))
-print("mnist dataset load done")
 
-# 读取到的数据区分训练集、验证集、测试集
+def load_data(mode="train"):
+    dataFile = "./work/mnist.json.gz"
+    print("loading mnist dataset from {} ......".format(dataFile))
+    # 加载 json 文件
+    data = json.load(gzip.open(dataFile))
+    print("mnist dataset load done")
 
-train_set, val_set, eval_set = data
+    # 读取到的数据区分训练集，验证集和测试集
+    train_set, val_set, eval_set = data
+    if mode == "train":
+        imgs, labels = train_set[0], train_set[1]
+    elif mode == "valid":
+        imgs, labels = val_set[0], val_set[1]
+    elif mode == "eval":
+        imgs, labels = eval_set[0], eval_set[1]
+    else:
+        raise Exception("mode can only be one of ['train', 'valid', 'eval']")
+    print("训练数据集数量: ", len(imgs))
 
-# 观察训练集数据
-imgs, labels = train_set[0], train_set[1]
-print("训练数据集数量:", len(imgs))
+    # 校验数据
+    imgs_length = len(imgs)
 
-# # 观察验证集数量
-# imgs, labels = val_set[0], val_set[1]
-# print("验证数据集数量:", len(imgs))
+    assert len(imgs) == len(labels), \
+        "length of train_imgs({}) should be the same as train_labels({})".format(
+            len(imgs), len(labels))
 
-# # 观察测试集数量
-# imgs, labels = eval_set[0], eval_set[1]
-# print("测试数据集数量:", len(imgs))
+    # 获取数据集长度
+    imgs_length = len(imgs)
 
-# 获取数据集长度
-imgs_length = len(imgs)
-# 定义数据集每个数据的序号，根据序号读取数据
-index_list = list(range(imgs_length))
-# 读入数据时用到的批次大小
-BATCHSIZE = 100
+    # 定义数据集每个数据的序号，根据序号读取数据
+    index_list = list(range(imgs_length))
+    # 读入数据时用到的批次大小
+    BATCHSIZE = 100
 
-# 随机打乱训练数据的索引序号
-random.shuffle(index_list)
+    # 定义数据生成器
+    def data_generator():
+        if mode == "train":
+            # 训练模式下打乱数据
+            random.shuffle(index_list)
+        imgs_list = []
+        labels_list = []
+        for i in index_list:
+            # 将数据处理成希望的类型
+            img = np.array(imgs[i]).astype("float32")
+            label = np.array(labels[i]).astype("float32")
+            imgs_list.append(img)
+            labels_list.append(label)
+            if len(imgs_list) == BATCHSIZE:
+                # 获得一个 batchsize 的数据并返回
+                yield np.array(imgs_list), np.array(labels_list)
+                # 清空数据读取列表
+                imgs_list = []
+                labels_list = []
 
-# 定义数据生成器,返回批次数据
-def data_generator():
-    imgs_list = []
-    labels_list = []
-    for i in index_list:
-        # 将数据处理成希望的类型
-        img = np.array(imgs[i]).astype("float32")
-        label = np.array(labels[i]).astype("float32")
-        imgs_list.append(img)
-        labels_list.append(label)
-        if len(imgs_list) == BATCHSIZE:
-            # 获得一个 batchsize 的数据，并返回
-            yield np.array(imgs_list),np.array(labels_list)
-            # 清空数据读取列表
-            imgs_list = []
-            labels_list = []
-    
-    # 如果剩余数据的数目小于 BATCHSIZE
-    # 则剩余数据一起构成一个大小为 len(imgs_list) 的 mini-batch
-    if len(imgs_list) > 0:
-        yield np.array(imgs_list), np.array(labels_list)
+        # 如果剩余数据的数目小于 BATCHSIZE
+        # 则剩余数据一起构成一个大小为 len(imgs_list) 的 mini-batch
+        if len(imgs_list) > 0:
+            yield np.array(imgs_list), np.array(labels_list)
     return data_generator
 
-# 声明数据读取函数，从训练集中读取数据
-train_loader = data_generator
-# 以迭代的形式读取数据
-for batch_id,data in enumerate(train_loader()):
-    image_data,label_data = data
-    if batch_id == 0:
-        # 打印数据 shape 和 类型
-        print("打印第一个batch数据的维度:")
-        print("图像维度: {}, 标签维度: {}".format(image_data.shape, label_data.shape))
-    break
+# 数据处理部分之后的代码，数据读取的部分调用 Load_data 函数
+# 定义网络结构，同上一节所使用的网络结构
+
+
+class MNIST (paddle.nn.Layer):
+    def __init__(self):
+        super(MNIST, self).__init__()
+        # 定义一层全链接层，输出维度是1
+        self.fc = paddle.nn.Linear(in_features=784, out_features=1)
+    def forward(self,inputs):
+        outputs = self.fc(inputs)
+        return outputs
+
+# 训练配置，并启动训练过程
+def train(model):
+    model = MNIST()
+    model.train()
+    # 调用加载数据的函数
+    train_loader = load_data("train")
+    opt = paddle.optimizer.SGD(learning_rate = 0.001,parameters = model.parameters())
+    EPOCH_NUM = 10
+    for epoch_id in range(EPOCH_NUM):
+        for batch_id,data in enumerate(train_loader()):
+            # 准备数据，变得更加简洁
+            images,labels = data
+            images = paddle.to_tensor(images)
+            labels = paddle.to_tensor(labels)
+
+            # 前向计算过程
+            predits = model(images)
+
+            # 计算损失,取一个批次样本损失的平均值
+
+            loss = F.square_error_cost(predits,labels)
+            avg_loss = paddle.mean(loss)
+
+            # 每训练 200 批次的数据，打印下当前 Loss 的情况
+            if batch_id % 200 == 0:
+                print("epoch: {},batch: {},loss is : {}".format(epoch_id,batch_id,avg_loss.numpy()))
+
+            # 后向传播，更新参数的过程
+            avg_loss.backward()
+            opt.step()
+            opt.clear_grad()
+    # 保存模型
+    paddle.save(model.state_dict(),"./mnist.pdparams")
+
+# 创建模型
+model = MNIST()
+# 启动训练过程
+train(model)
